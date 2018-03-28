d7dc83f2d994e7bdbbd8c21de2b1e2116be123f1
Matthew Brown <mattbrown@Matthews-MBP.fios-router.home>
2018-03-28 08:35:06 -0400

    working stitcher commit

--- stitch.cpp
+++ stitch.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <fstream>
 
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/stitching/stitcher.hpp"
-#include "opencv2/core/core.hpp"
 #include "opencv2/opencv.hpp"
+#include <opencv2/xfeatures2d.hpp>
 
 using namespace std;
 using namespace cv;
@@ -12,19 +10,40 @@ using namespace cv;
 
 Mat crop( Mat img , int x1, int x2, int y1, int y2)
 {
-	cout << (x2-x1)/2 << " " << (y2-y1)/2 << " " << x2-x1 << " " << y2-y1 << endl;
 	Rect cropper = Rect(0,0,x2-x1, y2-y1);
-	cout<< "Built rect" << endl;
 	Mat cropped = img(cropper);
-	cout << "built cropped" << endl;
 	return cropped ; 
+}
+
+vector<KeyPoint> detectFeatures ( Mat img )
+{
+	int minHessian = 400;
+	static Ptr<xfeatures2d::SURF> detector = xfeatures2d::SURF::create(minHessian);
+	vector<KeyPoint> keypoints;
+	detector->detect ( img, keypoints);
+
+
+	return keypoints;
+}
+
+void showKeyPoints ( vector<vector<KeyPoint> > keys, vector<Mat> imgs )
+{
+	for ( int i = 0; i < imgs.size(); i++ )
+	{
+		Mat img_keys ;
+		drawKeypoints( imgs[i], keys[i], img_keys ) ;
+		stringstream ss;
+		ss << i;
+		imshow ( ss.str(), img_keys );
+	}
+	waitKey(0);
 }
 
 bool statuscheck(Stitcher::Status stat)
 {
 		if ( stat != Stitcher::OK)
 	{
-		cout <<"Can't stitch images\n" << endl;
+		cout << "status issue: " <<  stat << endl;
 		return false;
 	}
 	else
@@ -38,20 +57,48 @@ int main ( int argc, char** argv)
 
 	vector<Mat> imgs;
 	Stitcher stitcher = Stitcher::createDefault();
-
-	for ( int i = 1; i < 10; i++ )
+	/*
+	for ( int i = 1; i < 15; i++ )
 	{
 		stringstream ss;
-		ss << "../New folder/Hole_0" << i << ".jpg";
+		if ( i < 10 )
+		{
+			ss << "../brian-images/Hole_0" << i << ".jpg";
+		}
+		else
+		{
+			ss << "../brian-images/Hole_" << i << ".jpg";
+		}
 		
 		Mat img = imread(ss.str());
-		cout << img.cols << endl;
 		Mat cropped = crop(img,1, img.cols-1, 1, 965);
+		Mat reduced ;
+		resize (cropped, reduced, Size(), 0.5, 0.5) ;
+		imgs.push_back ( reduced );
+	}
+	*/
 
-		imgs.push_back(img);
+	for ( int i = 0; i < 5; i++)
+	{
+		stringstream ss;
+		if (i < 5 )
+		{
+			ss << "../brian-images/Hole_2_" << i+2 << ".jpg";
+		}
+		else
+		{
+			ss << "../brian-images/Hole_9_" << i-3 << ".jpg";
+		}
+		cout << ss.str() << endl;
+		Mat img = imread(ss.str());
+		Mat cropped = crop(img,1, img.cols-1, 1, 965);
+		Mat reduced ;
+		resize (cropped, reduced, Size(), 0.5, 0.5) ;
+		imgs.push_back ( reduced );
 	}
 
 	Mat pano;
+
 	Stitcher::Status status = stitcher.stitch(imgs, pano);
 	bool s =statuscheck(status);
 
@@ -59,7 +106,7 @@ int main ( int argc, char** argv)
 		return -1;
 	else
 	{
-		imwrite("stitched.jpg", pano);
+		imwrite("../stitched.jpg", pano);
 		imshow("stitched", pano);
 		waitKey(0);
 		return 0;
